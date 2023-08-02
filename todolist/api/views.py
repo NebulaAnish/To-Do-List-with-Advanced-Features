dbf574277a5d88f2a85620309135fa28fb852176
Nebula <nebulaanish@gmail.com>
2023-08-02 07:30:21 +0545

    CRUD, Search, Pagination and Filtering implemented

---
+++ todolist/api/views.py
@@ -0,0 +1,44 @@
+from rest_framework import generics, filters
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+
+from .models import ToDoItem
+from .serializers import ToDoItemSerializer
+from django.utils.decorators import method_decorator
+import django_filters
+
+class ToDoItemListView(generics.ListCreateAPIView):
+    queryset = ToDoItem.objects.all()
+    serializer_class = ToDoItemSerializer
+    pagination_class = PageNumberPagination
+    filter_backends= [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ['id','title', 'description', 'completed']
+    search_fields = ['id','title', 'description', 'completed']
+    ordering_fields = ['id','title', 'description', 'completed']
+
+
+class ToDoItemRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ToDoItem.objects.all()
+    serializer_class = ToDoItemSerializer
+
+@api_view(['GET'])
+def ApiOverview(request):
+    # api_urls = {
+    #     'To Do Items':'todolist/',
+    #     'Search by Title':'/?title=title',
+    #     'Search by Description':'/?description=description',
+    #     'Add':'/create',
+    #     'Update':'/item/pk/update',
+    # }
+    api_urls = {
+        'List To Do Items With Pagination and Search Functionality using query parameters':'GET /items',
+        'Create':'POST /items/',
+        'Retrieve':'GET /items/<id>/',
+        'Update':'PUT /items/<id>/',
+        'Delete': 'DELETE /items/<id>/'
+
+    }
+    return Response(api_urls)
